2054d2f12b4cd573856ed02620363dbabda92381
Elizabeth-Munywe <elizabeth.munywe@griffinglobaltech.com>
2026-04-17 15:57:09 +0300

    navigation.cy

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -2,39 +2,43 @@ describe("Icebreaker", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173");
   });
+
   it("should show categories on the home page", () => {
-    cy.get('[data-cy="category-card"]', { timeout: 2000 }).should(
-      "have.length.greaterThan",
-      0,
-    );
+    cy.get('[data-cy="category-card"]', { timeout: 10000 })
+      .should("have.length.greaterThan", 0);
   });
 
   it("should navigate through cards and return home", () => {
-    cy.get('[data-cy="category-card"]', { timeout: 2000 }).should(
-      "have.length.greaterThan",
-      0,
-    );
-    cy.wait(1500);
+   
+    cy.get('[data-cy="category-card"]', { timeout: 10000 })
+      .should("have.length.greaterThan", 0);
 
-    cy.get('[data-cy="category-card"]').first().click();
-    cy.wait(2000);
-
-    cy.contains("Next").click();
-    cy.wait(1500);
-
-    cy.contains("Previous").click();
-    cy.wait(1500);
-
+   
     cy.get('[data-cy="category-card"]').first().click();
 
-    cy.get('[data-cy="next-question"]').should("be.visible").click();
+    
+    cy.get('[data-cy="next-question"]', { timeout: 10000 })
+      .should("exist");
 
-    cy.get('[data-cy="previous-question"]').should("be.visible").click();
+    
+    cy.get('[data-cy="next-question"]')
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
 
-    cy.contains("Back").click();
+    
+    cy.get('[data-cy="previous-question"]')
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
 
-    cy.url().should("include", "localhost:5173");
+    
+    cy.contains("Back").should("be.visible").click();
 
+    
+    cy.url().should("eq", "http://localhost:5173/");
+
+    
     cy.get('[data-cy="category-card"]').should("be.visible");
   });
 });
